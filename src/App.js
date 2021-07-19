95b629d8cf3390b65b11da77277252c8a9a9e647
Eugene Molari <eugelogic@gmail.com>
2021-07-19 16:20:19 +0100

    Draft Interval widget

--- src/App.js
+++ src/App.js
@@ -4,23 +4,43 @@ import Player from './Player';
 function App() {
   return (
     <div>
-      <header>
-        <h1>
-          Music Intervals
-        </h1>
+
+      <div>
+
         <div>
-          <h2>Sample #1</h2>
-        <Player src="http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_shoot.wav" />
+
+        <div>
+            <span>"Name of the Song"</span>
         </div>
+
         <div>
-          <h2>Sample #2</h2>
-        <Player src="/audio/Interval-minor 2nd.m4a" />
-        </div>
-        <div>
-          <h2>Sample #3</h2>
+          <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000">
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z" />
+          </svg>
           <Player src="/audio/sample.mp3" />
         </div>
-      </header>
+
+        <div>
+          <span>minor 2nd</span>
+        </div>
+
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000">
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
+          </svg>
+          <Player src="/audio/Interval-minor 2nd.m4a" />
+        </div>
+
+        <div>
+            <span>"Name of the Song"</span>
+        </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
